f3c8603f2df0b369aea6da043fa3765e3af112cb
Passwd <passwd@mes3hacklab.org>
2019-03-18 19:13:16 +0100

    fixed some createdb

--- src/invoicemanager/persistence/fatturazione/WriteAllegato.java
+++ src/invoicemanager/persistence/fatturazione/WriteAllegato.java
@@ -3,6 +3,7 @@ package invoicemanager.persistence.fatturazione;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import invoicemanager.model.fatturazione.Allegato;
 
@@ -15,8 +16,7 @@ public class WriteAllegato {
 
 	public void add(Allegato a, boolean exist) throws ClassNotFoundException, SQLException {
 	    try {
-
-			PreparedStatement ps = c.prepareStatement("INSERT INTO Allegato VALUES (?, ?,)");
+			PreparedStatement ps = c.prepareStatement("INSERT INTO Allegato VALUES (DEFAULT, ?, ?)");
 			ps.setString(1, a.getNomeAllegato());
 			ps.setString(2, a.getPathAllegato());
 	
@@ -24,8 +24,26 @@ public class WriteAllegato {
 			ps.close();
 			c.commit();
 			c.close();
-			} catch (Exception e) {
-			  //Utils.createAlertFailWriteDB();
-			}
+		} catch (Exception e) {
+		  //Utils.createAlertFailWriteDB();
 		}
+	}
+	
+	public void createDB() {
+		try {
+        Statement stmt = c.createStatement();
+    	String sql = "Create table public.Allegato (\r\n" +
+    			"	\"id\" serial NOT NULL primary key,\r\n" + 
+    			"	\"NomeAllegato\" varchar(25) NOT NULL,\r\n" + 
+    			"	\"PathAllegato\" varchar(25) NULL\r\n" + 
+    			"); ";		//AGGIUNGERE STATO
+    	stmt.executeUpdate(sql);
+    	stmt.close();
+        c.commit();
+        c.close();
+	} catch (Exception e) {
+		//Utils.createAlertFailWriteDB();
+	}
+
+	}
 }
--- src/invoicemanager/persistence/fatturazione/WriteBentoDettaglio.java
+++ src/invoicemanager/persistence/fatturazione/WriteBentoDettaglio.java
@@ -20,12 +20,12 @@ public class WriteBentoDettaglio {
 				PreparedStatement ps = c.prepareStatement("INSERT INTO BentoDettaglio VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,)");
 				ps.setString(1, a.getCodiceBento());
 				ps.setInt(2, a.getNumeroRigaBento());
-				ps.setString(3, a.getCodiceTipoBento().getCodiceTipoBento());
+				ps.setString(3, a.getCodiceTipoBento());
 				ps.setString(4, a.getCodiceRigaBento());
 				ps.setString(5, a.getCodiceArticolo());
 				ps.setString(6, a.getDescrizione());
 				ps.setFloat(7, a.getQuantita());
-				ps.setString(8, a.getCodiceUnitaMisura().getCodiceUnitaMisura());
+				ps.setString(8, a.getCodiceUnitaMisura());
 				ps.setFloat(9, a.getPrezzo());
 				ps.setString(10, a.getCodiceIva());
 				ps.setString(11, a.getCodiceControparte());
